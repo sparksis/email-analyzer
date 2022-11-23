60b4c82e0e5593121e54275b961a268d6015f036
colton <colton@sparksis.com>
2022-11-22 23:02:58 -0700

    super simple storage engine

---
+++ public/messages.mjs
@@ -0,0 +1,21 @@
+export const db = new Dexie('EmailDB');
+
+db.version(1).stores({
+    messages: 'id, from',
+});
+
+export async function findUniqueDomains() {
+    const seenDomains = [];
+    await db.messages.orderBy('from').eachUniqueKey(from => {
+        const domain = from.substring(1 + from.lastIndexOf('@'));
+        // Naive look at all domains will cause O(n^2).
+        // We should investigate a projection based approach
+        // with in -order insertions to ensure that we are O(n)
+        if (!seenDomains.includes(domain)) {
+            seenDomains.push(domain);
+        }
+    });
+    return seenDomains;
+}
+
+export default db.messages;
---
+++ public/messages.test.mjs
@@ -0,0 +1,62 @@
+import { default as messages, db, findUniqueDomains } from "./messages.mjs";
+
+
+const messagesFixture = [
+    { id: '1', from: '1@example.net' },
+    { id: '2', from: '2@example.net' },
+];
+
+describe('storage', function () {
+    beforeEach('reset the db', function () {
+        return db.open();
+    });
+    afterEach(async function () {
+        await db.close();
+        await new Promise((resolve, reject) => {
+            let request = window.indexedDB.deleteDatabase('EmailDB');
+            request.onsuccess = resolve;
+            request.onerror = reject;
+        })
+    });
+    it('will run', function () { });
+
+    // Just validating the library.
+    describe('db', function () {
+        it('should be accessable', async function () {
+            db.should.exist;
+        });
+        it('should be a mutable db', async function () {
+            await db.messages.bulkPut(messagesFixture);
+        });
+    });
+
+    describe('bulkPut', function () {
+        it('should be able to store some messages', async function () {
+            try {
+                await messages.bulkPut(messagesFixture);
+            } catch (error) {
+                throw error;
+            }
+        })
+    });
+
+    describe('findUniqueDomains', function () {
+        const domains = ['example.net', 'example.com'];
+        const emails = ['Django', 'Jeff', 'Tiana']
+            .reduce((acc, name) => {
+                domains.forEach(domain => acc.push(`${name}@${domain}`));
+                return acc;
+            }, []);
+        before('Setup database', async function () {
+            await db.open();
+            await messages.bulkPut(emails.map((v, i) => { return { id: i, from: v } }));
+        });
+        after('Close Database', async function () {
+            await db.close();
+        });
+        it(`should find ${domains.length} domains`, async function () {
+            const uniqueDomains = await findUniqueDomains();
+            uniqueDomains.should.include.all.members(domains);
+        });
+    });
+});
