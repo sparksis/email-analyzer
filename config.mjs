0dbe04f58e19b0b0b9375dfb90bf82096156929c
colton <colton@sparksis.com>
2022-12-03 16:31:27 -0700

    Adding github.io config

---
+++ config.mjs
@@ -0,0 +1,7 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+export const CLIENT_ID = '332915055902-9685d4vimjjc3hf4ev8fclab55rakn5a.apps.googleusercontent.com';
+export const API_KEY = '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';
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+export const SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
