7a5b53aa4464e544735fb7f24bfa02e142e8021a
colton <colton@sparksis.com>
2022-12-04 23:27:49 -0700

    re-adding poc code to validate ahead of time compilation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {handleLogin} from './poc/gmail.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"  />
+        <button type='button' onClick={handleLogin}  >Login</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
