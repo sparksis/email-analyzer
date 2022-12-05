049f0d1206529528348117c84900596440c4d998
colton <colton@sparksis.com>
2022-12-04 23:27:49 -0700

    re-adding poc code to validate ahead of time compilation

---
+++ src/poc/message.type.js
@@ -0,0 +1,45 @@
+function extractInternetAddress(address) {
+    const matches = address.match(/.*\<(.*)\>$/);
+    const internetAddress = matches && matches[1] || address;
+    return internetAddress.toLowerCase();
+}
+function parseDomain(internetAddress) {
+    try {
+        return internetAddress.substring(internetAddress.lastIndexOf('@') + 1);
+    } catch (e) {
+        return console.error('failed to parse domain for', this.from, e);
+    }
+}
+
+class Headers {
+    from;
+    to;
+    sent;
+    subject;
+    'delivered-to';
+    constructor(values) {
+        values.forEach(h => {
+            if (h.name)
+                this[h.name.toLowerCase()] = h.value
+        });
+    }
+}
+
+export default class Message {
+    constructor({ id, snippet, internalDate, payload }) {
+        this.id = id;
+        this.snippet = snippet;
+        this.recieved = internalDate;
+
+        const headers = new Headers(payload.headers);
+        this.from = extractInternetAddress(headers.from);
+        this.domain = parseDomain(this.from);
+        if (headers["delivered-to"]) {
+            this.to = extractInternetAddress(headers["delivered-to"]);
+        } else {
+            this.to = extractInternetAddress(headers.to);
+        }
+        this.subject = headers.subject;
+    }
+
+}
---
+++ src/poc/messages.js
@@ -0,0 +1,48 @@
+import {Dexie} from 'dexie';
+export const db = new Dexie('EmailDB');
+
+db.version(1).stores({
+    messages: 'id, to, domain, from, [to+domain+from]',
+});
+
+export async function findUniqueDomains() {
+    const seenDomains = [];
+    console.log('finding domains');
+    let lastSeen;
+    await db.messages.orderBy('domain').eachUniqueKey(domain => {
+        if (domain !== lastSeen) {
+            console.log('found a unique domain', domain);
+            seenDomains.push(domain);
+            lastSeen = domain;
+        }
+    });
+    return seenDomains;
+}
+
+export async function generateStats() {
+    const TO = 0, DOMAIN = 1, FROM = 2;
+    const stats = {};
+    const lastSeen = new Array(3);
+
+    await db.messages.orderBy('[to+domain+from]').eachKey(function (key) {
+        // console.log(...key);
+        if (key[TO] != lastSeen[TO]) {
+            lastSeen[TO] = key[TO];
+            stats[key[TO]] = { _count: 0 };
+        }
+        if (key[DOMAIN] != lastSeen[DOMAIN]) {
+            lastSeen[DOMAIN] = key[DOMAIN];
+            stats[key[TO]][key[DOMAIN]] = { _count: 0 };
+        }
+        if (key[FROM] != lastSeen[FROM]) {
+            lastSeen[FROM] = key[FROM];
+            stats[key[TO]][key[DOMAIN]][key[FROM]] = 0;
+        }
+        stats[key[TO]]._count += 1;
+        stats[key[TO]][key[DOMAIN]]._count += 1;
+        stats[key[TO]][key[DOMAIN]][key[FROM]] += 1;
+    });
+    return stats;
+}
+
+export default db.messages;
