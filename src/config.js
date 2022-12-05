882882956f86e1e55c66867cb7dee33310e7b285
colton <colton@sparksis.com>
2022-12-04 23:58:07 -0700

    Read secret from environment

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const API_KEY = process.env.REACT_APP_CLIENT_SECRET;
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+export const SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
